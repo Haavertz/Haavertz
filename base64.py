10bffb2a50012a9770f8a520d0bd655536027b30
Gleison <gleisonsenac06@gmail.com>
2024-01-22 11:27:19 -0300

    Base64 Convert Images

---
+++ base64.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import base64
+
+# pip install pillow
+
+caminho_da_imagem = "#"
+
+imagem = Image.open(caminho_da_imagem)
+
+with open(caminho_da_imagem, "rb") as imagem_arquivo:
+    base64_data = base64.b64encode(imagem_arquivo.read())
+
+base64_string = base64_data.decode('utf-8')
+
+print(base64_string)
