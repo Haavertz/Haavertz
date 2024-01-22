01a87d37a4baad2d4390ed17a5e36acc4bbede40
Gleison <gleisonsenac06@gmail.com>
2024-01-22 11:47:37 -0300

    Convert_Base64 and ADD Manhwa

--- Convert_base64.py
+++ Convert_base64.py
@@ -3,7 +3,7 @@ import base64
 
 # pip install pillow
 
-caminho_da_imagem = "#"
+caminho_da_imagem = "C:/Users/gleis/OneDrive/Documentos/GitHub/About_Me/264156.jpg"
 
 imagem = Image.open(caminho_da_imagem)
 
